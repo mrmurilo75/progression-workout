386a10b3d31f26ea8cdd4b5d7acd2c30138a4a64
mrmurilo75 <mrmurilo75@gmail.com>
2022-10-31 11:45:45 +0000

    add core app

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "progression.users",
     # Your stuff: custom apps go here
+    "progression.core",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
