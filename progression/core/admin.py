d38cc37175f0003249b72748cb484e1c7b897565
Murilo Rosa <mrmurilo75@gmail.com>
2023-05-29 10:30:56 +0100

    restructure models

--- progression/core/admin.py
+++ progression/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import TrainingDay, TrainingExercise
+from .models import Exercise, ExerciseConfig, Plan, TrainingDay
 
 admin.site.register(TrainingDay)
-admin.site.register(TrainingExercise)
+admin.site.register(ExerciseConfig)
+admin.site.register(Exercise)
+admin.site.register(Plan)
--- progression/core/models.py
+++ progression/core/models.py
@@ -1,18 +1,40 @@
 from django.db import models
 
 
-class TrainingDay(models.Model):
-    name = models.CharField(max_length=127)
+# We have a Plan
+#           |_ Have a few Days
+#                          |_ Each a set of Compound Exercise -> compromised of
+#                                       [Exercise][Weight][Repetitions][Series]
+#                                           -> The same exercise can have more then 1 configuration,
+#                                              but it should still be the same exercise
 
 
-class TrainingExercise(models.Model):
-    training_day = models.ForeignKey(
-        TrainingDay,
-        on_delete=models.CASCADE,
-    )
+class Exercise(models.Model):
     name = models.CharField(max_length=127)
     description = models.TextField(max_length=1023, blank=True)
+    # FEAT Add more media fields for better description
+
+
+class ExerciseConfig(models.Model):
+    exercise = models.ForeignKey(
+        Exercise,
+        on_delete=models.PROTECT,
+    )
     weight = models.IntegerField()
     repetitions = models.SmallIntegerField(default=3)
     series = models.SmallIntegerField(default=3)
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=127)
+
+
+class TrainingDay(models.Model):
+    plan = models.ForeignKey(
+        Plan,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=127)
+    exercises = models.ManyToManyField(ExerciseConfig)
+
 
